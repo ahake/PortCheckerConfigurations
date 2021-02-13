1ed1a30717168fcf924232901b255bf1adbfd9ce
Andy Hake <ahake@users.noreply.github.com>
2021-02-13 23:41:39 +1100

    Autostore Script added

---
+++ Check_AutoStore_Ports.ps1
@@ -0,0 +1,20 @@
+Import-Module .\Test-Ports
+
+$ErrorActionPreference = Stop
+[String]$server = "127.0.0.1"
+[String]$pcServer = ""
+
+Clear-Host
+
+Write-Host "---------------------------------------------------------------------------------------------------------------------------------------------------"
+Write-Host "This script checks Port Connectivity on an AutoStore Server"
+Write-Host "Please run this script on the AutoStore server."
+Write-Host "---------------------------------------------------------------------------------------------------------------------------------------------------"
+Write-Host "Ports should return `"LISTENING`""
+Write-Host "---------------------------------------------------------------------------------------------------------------------------------------------------"
+
+Write-PortTest -server $server -port 8085 
+Write-PortTest -server $server -port 3242
+Write-PortTest -server $server -port 3281
+Write-PortTest -server $server -port 25
+Write-PortTest -server $server -port 3290
