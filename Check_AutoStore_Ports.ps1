2d8676e5027f43b1c7faa70e44e86aae871fccec
Andy Hake <andy.hake@viatek.com.au>
2021-02-15 14:35:27 +1100

    AutoStore Core

--- Check_AutoStore_Ports.ps1
+++ Check_AutoStore_Ports.ps1
@@ -1,20 +1,30 @@
-Import-Module .\Test-Ports
+#Requires -PSEdition Core
+#Requires -Version 6.1
 
 $ErrorActionPreference = Stop
-[String]$server = "127.0.0.1"
-[String]$pcServer = ""
 
 Clear-Host
 
-Write-Host "---------------------------------------------------------------------------------------------------------------------------------------------------"
-Write-Host "This script checks Port Connectivity on an AutoStore Server"
-Write-Host "Please run this script on the AutoStore server."
-Write-Host "---------------------------------------------------------------------------------------------------------------------------------------------------"
-Write-Host "Ports should return `"LISTENING`""
-Write-Host "---------------------------------------------------------------------------------------------------------------------------------------------------"
+Write-Host "---------------------------------------------------------------"
+Write-Host "This script checks Port Connectivity on an AutoStore Server   -"
+Write-Host "Please run this script on the AutoStore server.               -"
+Write-Host "---------------------------------------------------------------"
 
-Write-PortTest -server $server -port 8085 
-Write-PortTest -server $server -port 3242
-Write-PortTest -server $server -port 3281
-Write-PortTest -server $server -port 25
-Write-PortTest -server $server -port 3290
+Start-Job -Name "8085" -ScriptBlock { Test-Connection -ComputerName 127.0.0.1 -TcpPort 8085 } | Out-Null
+Start-Job -Name "3242" -ScriptBlock { Test-Connection -ComputerName 127.0.0.1 -TcpPort 3242 } | Out-Null
+Start-Job -Name "3281" -ScriptBlock { Test-Connection -ComputerName 127.0.0.1 -TcpPort 3281 } | Out-Null
+Start-Job -Name "25" -ScriptBlock { Test-Connection -ComputerName 127.0.0.1 -TCPPort 25 } | Out-Null
+Start-Job -Name "3290" -ScriptBlock { Test-Connection -ComputerName 127.0.0.1 -TCPPort 3290 } | Out-Null
+
+$p8085 = Receive-Job -Name "8085" -wait
+$p3242 = Receive-Job -Name "3242" -wait
+$p3281 = Receive-Job -Name "3281" -wait
+$p0025 = Receive-Job -Name "25" -wait
+$p3290 = Receive-Job -Name "3290" -wait
+
+
+Write-Host "Port 8085: " $p8085
+Write-Host "Port 3242: " $p3242
+Write-Host "Port 3281: " $p3281
+Write-Host "Port   25: " $p0025
+Write-Host "Port 3290: " $p3290
